3519ace70248130a350a964c8fe9de430651d23e
Timothy Meixell <misternu@gmail.com>
2017-10-15 14:56:50 -0500

    first attempt

--- game.exs
+++ game.exs
@@ -1,7 +1,34 @@
 defmodule GuessingGame do
 
-  def hi() do
-    IO.puts("hello world")
+  def puts_high_low(n, t) do
+    if (n < t) do
+      IO.puts("Too Low")
+    else
+      IO.puts("Too High")
+    end
   end
 
+  def win() do
+    IO.puts("That's the right number, you win!")
+  end
+
+  def get_guess() do
+    String.to_integer(String.trim(IO.gets("Guess a number: ")))
+  end
+
+  def play() do
+    main(Enum.random(1..100))
+  end
+
+  def main(number) do
+    guess = get_guess()
+    if (guess == number) do
+      win()
+    else
+      puts_high_low(guess, number)
+      main(number)
+    end
+  end
 end
+
+GuessingGame.play()
