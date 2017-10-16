f864849eaadb783d8e8507c30fe49ddff2d6ad82
Timothy Meixell <misternu@gmail.com>
2017-10-16 16:27:48 -0500

    Second refactor

--- game.exs
+++ game.exs
@@ -1,16 +1,6 @@
 defmodule GuessingGame do
 
   def compare(n, t) do
-    # Best Practice ?
-
-    # a)
-    # case n do
-    #   m when m < t -> :low
-    #   m when m == t -> :equal
-    #   m when m > t -> :high
-    # end
-
-    # b)
     cond do
       n < t -> :low
       n == t -> :equal
@@ -18,41 +8,32 @@ defmodule GuessingGame do
     end
   end
 
-  def print_state(state) do
-    case state do
-      :low -> IO.puts("Too low")
-      :equal -> IO.puts("That's the right number, you win!")
-      :high -> IO.puts("Too high")
-    end
-  end
-
-  def print_turns(turns) do
-    IO.puts("You have #{turns} turns left")
-    if (turns == 0) do
-      IO.puts("You lose")
-    end
-  end
+  def print_state(:low),      do: IO.puts("Too low")
+  def print_state(:equal),    do: IO.puts("That's the right number, you win!")
+  def print_state(:high),      do: IO.puts("Too high")
+  # def print_state(direction), do: IO.puts("Too #{direction}") # Too clever?
+  def print_turns(0),         do: IO.puts("You have 0 turns left, You lose")
+  def print_turns(turns),     do: IO.puts("You have #{turns} turns left")
 
   def get_guess() do
-    IO.gets("Guess a number: ") |> String.trim |> String.to_integer
+    IO.gets("Guess a number: ")
+      |> String.trim
+      |> String.to_integer
   end
 
-  def play() do
-    main(Enum.random(1..100), 7)
-  end
-
-  def turn(number) do
-    state = get_guess() |> compare(number)
-    print_state(state)
-    state == :equal
-  end
+  def play(), do: main(Enum.random(1..100), 7)
 
   def main(number, turns) do
     print_turns(turns)
-    if (turns > 0 && !turn(number)) do
-      main(number, turns-1)
+    if (turns > 0) do
+      state = get_guess() |> compare(number)
+      print_state(state)
+      if (state != :equal) do
+        main(number, turns-1)
+      end
     end
   end
+
 end
 
 GuessingGame.play()
