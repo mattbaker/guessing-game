784a3c041ee491e389fd0a8bd122a414a6067e99
Timothy Meixell <misternu@gmail.com>
2017-10-16 09:00:40 -0500

    First refactor

--- game.exs
+++ game.exs
@@ -1,38 +1,53 @@
 defmodule GuessingGame do
 
-  def puts_high_low(n, t) do
-    if (n < t) do
-      IO.puts("Too Low")
-    else
-      IO.puts("Too High")
+  def compare(n, t) do
+    # Best Practice ?
+
+    # a)
+    # case n do
+    #   m when m < t -> :low
+    #   m when m == t -> :equal
+    #   m when m > t -> :high
+    # end
+
+    # b)
+    cond do
+      n < t -> :low
+      n == t -> :equal
+      n > t -> :high
     end
   end
 
-  def win() do
-    IO.puts("That's the right number, you win!")
+  def print_state(state) do
+    case state do
+      :low -> IO.puts("Too low")
+      :equal -> IO.puts("That's the right number, you win!")
+      :high -> IO.puts("Too high")
+    end
   end
 
   def get_guess() do
-    input = String.trim(IO.gets("Guess a number: "))
-    if Regex.match?(~r/^[0-9]+$/, input) do
-      String.to_integer(input)
-    else
-      IO.puts("Input only digits please (0-9)")
-      get_guess()
-    end
+    IO.gets("Guess a number: ") |> String.trim |> String.to_integer
   end
 
   def play() do
-    main(Enum.random(1..100))
+    main(Enum.random(1..100), 7)
   end
 
-  def main(number) do
-    guess = get_guess()
-    if (guess == number) do
-      win()
+  def turn(number) do
+    state = get_guess() |> compare(number)
+    print_state(state)
+    state == :equal
+  end
+
+  def main(number, turns) do
+    IO.puts("You have #{turns} turns left")
+    if (turns > 0) do
+      if (!turn(number)) do
+        main(number, turns-1)
+      end
     else
-      puts_high_low(guess, number)
-      main(number)
+      IO.puts("You lose")
     end
   end
 end
