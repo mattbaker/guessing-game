33c5c38252d8ec1c43f9cdf2bb963d61f0d66857
Timothy Meixell <misternu@gmail.com>
2017-10-21 16:06:34 -0500

    follow suggestion

--- game.exs
+++ game.exs
@@ -2,18 +2,22 @@ defmodule GuessingGame do
 
   def compare(n, t) do
     cond do
-      n < t -> :low
+      n < t  -> :low
       n == t -> :equal
-      n > t -> :high
+      n > t  -> :high
     end
   end
 
-  def print_state(:low),      do: IO.puts("Too low")
-  def print_state(:equal),    do: IO.puts("That's the right number, you win!")
-  def print_state(:high),      do: IO.puts("Too high")
-  # def print_state(direction), do: IO.puts("Too #{direction}") # Too clever?
-  def print_turns(0),         do: IO.puts("You have 0 turns left, You lose")
-  def print_turns(turns),     do: IO.puts("You have #{turns} turns left")
+  def print_outcome(state) do
+    case state do
+      :low   -> IO.puts("Too low")
+      :equal -> IO.puts("That's the right number, you win!")
+      :high  -> IO.puts("Too high")
+    end
+  end
+
+  def print_turns(0),     do: IO.puts("You have 0 turns left, You lose")
+  def print_turns(turns), do: IO.puts("You have #{turns} turns left")
 
   def get_guess() do
     IO.gets("Guess a number: ")
@@ -23,14 +27,13 @@ defmodule GuessingGame do
 
   def play(), do: main(Enum.random(1..100), 7)
 
+  def main(_, 0), do: print_turns(0)
   def main(number, turns) do
     print_turns(turns)
-    if (turns > 0) do
-      state = get_guess() |> compare(number)
-      print_state(state)
-      if (state != :equal) do
-        main(number, turns-1)
-      end
+    outcome = get_guess() |> compare(number)
+    print_outcome(outcome)
+    if (outcome != :equal) do
+      main(number, turns-1)
     end
   end
 
