e8fb850c73c1f2340675bb5e770cb7250c4f1f06
Tom McHenry <tommchenry@gmail.com>
2017-11-04 17:05:27 -0500

    Privatize methods, perform single guess iteration.

--- game.exs
+++ game.exs
@@ -1,16 +1,29 @@
 defmodule GuessingGame do
+  
+  def runner do
+    start_game
+    answer = generate_answer
+    get_user_guess
+    |>check_user_answer(answer)
+  end
 
-  #TODO: Greet player, explain game
+  defp start_game do
+    IO.puts("-=Fun Elixir Guessing Game=-")
+    IO.puts("I'll pick a number between 1 and 100 and you try to guess it.")
+  end
+
   defp generate_answer do
     :rand.uniform(100)
   end
-  #TODO: Get user guess
 
+  defp get_user_guess do
+    guess = IO.gets "What is your guess?\n"
+    String.replace(guess, "\n", "")
+    |> String.to_integer
+  end
 
   defp check_user_answer(guess, answer) do
     cond do
-      is_integer(guess) == false ->
-        IO.puts("Please guess a number within 1 and 100")
       guess > 100 or guess < 0 ->
         IO.puts("Please guess within 1 and 100")
       guess < answer ->
@@ -24,8 +37,6 @@ defmodule GuessingGame do
 
   #TODO: End game if answer correct
   #TODO: Implement chances
-
 end
 
-GuessingGame.generate_answer |> IO.puts
-
+GuessingGame.runner
