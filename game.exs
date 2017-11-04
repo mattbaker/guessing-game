f235895d6d178d332659453bc4e2becdc31e17f2
Tom McHenry <tommchenry@gmail.com>
2017-11-04 16:36:18 -0500

    Add pseudocode, refactor with cond statement

--- game.exs
+++ game.exs
@@ -1,30 +1,30 @@
 defmodule GuessingGame do
 
-  def hi() do
-    IO.puts("hello world")
+  #TODO: Greet player, explain game
+  #TODO: Pick random number
+  def generate_answer do
+    :rand.uniform(100)
+  end
+  #TODO: Get user guess
+
+
+  defp check_user_answer(guess, answer) do
+    cond do
+      is_integer(guess) == false ->
+        IO.puts("Please guess a number within 1 and 100")
+      guess > 100 or guess < 0 ->
+        IO.puts("Please guess within 1 and 100")
+      guess < answer ->
+        IO.puts("Sorry, #{guess} is too low.")
+      guess > answer ->
+        IO.puts("Sorry, #{guess} is too high.")
+      guess == answer ->
+        IO.puts("Correct! #{guess} is the correct answer")
+    end
   end
 
-  def check_user_answer(guess, answer) when guess > 100 or guess < 0 do
-    IO.puts("Please guess within 1 and 100")
-  end
-
-  def check_user_answer(guess, answer) when guess == answer do
-    IO.puts("Correct! #{guess} is the correct answer")
-  end
-
-  def check_user_answer(guess, answer) when guess < answer do
-    IO.puts("Sorry, #{guess} is too low.")
-  end
-
-  def check_user_answer(guess, answer) when guess > answer do
-    IO.puts("Sorry, #{guess} is too high.")
-  end
+  #TODO: End game if answer correct
+  #TODO: Implement chances
 
 end
 
-GuessingGame.check_user_answer(3,3)
-GuessingGame.check_user_answer(1,3)
-GuessingGame.check_user_answer(10,3)
-GuessingGame.check_user_answer(-1,3)
-GuessingGame.check_user_answer(300,3)
-GuessingGame.check_user_answer("a",3)
